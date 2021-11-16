964cc6b0d7803ff15416a305e226ec8635d6bafd
Chloe Harris <chloe.a.harris17@gmail.com>
2021-11-16 13:55:33 -0800

    Fully functional app

--- routes/api.js
+++ routes/api.js
@@ -11,13 +11,6 @@ router.get("/api/workouts", (req, res) => {
             }
         }
     }]).then(dbWorkout => {
-        // dbWorkout.forEach(workout => {
-        //     let total = 0;
-        //     workout.exercises.forEach(e => {
-        //         total += e.duration;
-        //     });
-        //     workout.totalDuration = total;
-        // });
         res.json(dbWorkout);
     }).catch((err) => {
         res.json(err);
@@ -27,7 +20,7 @@ router.get("/api/workouts", (req, res) => {
 // Add exercise
 router.put("/api/workouts/:id", ({ body, params }, res) => {
     Workout.findByIdAndUpdate(
-            params.id, { push: { exercises: body } }, { new: true, runValidators: true }
+            params.id, { $push: { exercises: body } }, { new: true, runValidators: true }
         )
         .then((dbWorkout) => {
             res.json(dbWorkout);
@@ -47,22 +40,6 @@ router.post("/api/workouts", ({ body }, res) => {
 
 });
 
-// Get aggregate duration
-// router.get("/api/workouts", (req, res) => {
-//     Workout.aggregate([{
-//             $addFields: {
-//                 totalDuration: {
-//                     $sum: '$exercises.duration',
-//                 },
-//             },
-//         }, ])
-//         .then((dbWorkouts) => {
-//             res.json(dbWorkouts);
-//         })
-//         .catch((err) => {
-//             res.json(err);
-//         });
-// });
 
 // Get workouts in range
 router.get("/api/workouts/range", (req, res) => {
@@ -81,15 +58,5 @@ router.get("/api/workouts/range", (req, res) => {
         });
 });
 
-// Delete workout
-router.delete("/api/workouts", ({ body }, res) => {
-    Workout.findByIdAndDelete(body.id)
-        .then(() => {
-            res.json(true);
-        })
-        .catch((err) => {
-            res.json(err);
-        });
-});
 
 module.exports = router;
